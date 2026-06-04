948556f0a1f8e15b0324051eb8f4e82c3885df6b
Efimova Ekaterina <k-9933@mail.ru>
2026-06-04 22:16:37 +0300

    доработка админки

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,6 +1,38 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 User = get_user_model()
-admin.site.register(User)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """Админка для кастомной модели User с email как USERNAME_FIELD."""
+
+    search_fields = ('email', 'username')
+
+    list_display = (
+        'id',
+        'email',
+        'username',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'is_active',
+    )
+
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Персональная информация', {'fields': ('username', 'first_name', 'last_name', 'avatar')}),
+        ('Права доступа', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Важные даты', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
